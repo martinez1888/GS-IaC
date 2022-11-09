95807921cc6eb33893e023d133c9b58faf872e42
martinez1888 <78212114+martinez1888@users.noreply.github.com>
2022-11-09 20:48:50 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,5 +22,10 @@ module "vpc" {
 
 module "Camada1" {
   source            = "./modules/Camada1"
+  sn_vpcgs_1a_id     = module.vpc.sn_vpcgs_1a_id
+  sn_vpcgs_1c_id     = module.vpc.sn_vpcgs_1c_id
+  sg_pub_id        = module.Camada1.sg_pub_id 
+  sg_priv1_id        = module.Camada1.sg_priv1_id 
+  sg_priv2_id        = module.Camada1.sg_priv2_id 
 
 }
