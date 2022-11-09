5fe48abc29acc62bb91733e6242d87627a2ff5e5
martinez1888 <78212114+martinez1888@users.noreply.github.com>
2022-11-09 19:53:33 -0300

    Update vars.tf

--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -1,1 +1,39 @@
+#VPC
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
 
+variable "vpc_dns_hostnames" {
+  default = true
+}
+
+# SUBNETS
+variable "sn_vpcgs_1a_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "sn_vpcgs_1c_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "sn_vpcgs_2a_cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "sn_vpcgs_2c_cidr" {
+  default = "10.0.4.0/24"
+}
+
+
+variable "sn_vpcgs_3a_cidr" {
+  default = "10.0.5.0/24"
+}
+
+variable "sn_vpcgs_3c_cidr" {
+  default = "10.0.6.0/24"
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  default     = "true"
+}
