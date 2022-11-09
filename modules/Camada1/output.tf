ea68b32127fb91e7fcf46ca0b763220ae4abe8b2
martinez1888 <78212114+martinez1888@users.noreply.github.com>
2022-11-09 20:32:45 -0300

    Update output.tf

--- modules/Camada1/output.tf
+++ modules/Camada1/output.tf
@@ -1,1 +1,12 @@
+#SECURITY GROUPS
+output "sg_pub_id" {
+  value = aws_security_group.sg_pub.id
+}
 
+output "sg_priv1_id" {
+  value = aws_security_group.sg_priv1.id
+}
+
+output "sg_priv2_id" {
+  value = aws_security_group.sg_priv2.id
+}
