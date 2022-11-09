b919403a710de113388244c6896fff334d2e9d1a
martinez1888 <78212114+martinez1888@users.noreply.github.com>
2022-11-09 19:52:02 -0300

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,1 +1,36 @@
+#VPC
+output "vpc_id" {
+  value = aws_vpc.vpcgs.id
 
+}
+
+#SUBNET
+output "sn_vpcgs_1a_id" {
+  value = aws_subnet.sn_vpcgs_1a.id
+
+}
+
+output "sn_vpcgs_1c_id" {
+  value = aws_subnet.sn_vpcgs_1c.id
+
+}
+
+output "sn_vpcgs_2a_id" {
+  value = aws_subnet.sn_vpcgs_2a.id
+
+}
+
+output "sn_vpcgs_2c_id" {
+  value = aws_subnet.sn_vpcgs_2c.id
+
+}
+
+output "sn_vpcgs_3a_id" {
+  value = aws_subnet.sn_vpcgs_3a.id
+
+}
+
+output "sn_vpcgs_3c_id" {
+  value = aws_subnet.sn_vpcgs_3c.id
+
+}
